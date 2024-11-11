eecbfc2b68a946cbb9cc979d2507d2c5f73056e1
Kirill <kirillpozmogov017@gmail.com>
2024-11-11 22:16:24 +0300

    added echo bot and tests

--- bot/test_bot.py
+++ bot/test_bot.py
@@ -1,2 +1,15 @@
-def test_bot():
-    assert 1 == 1
+import os
+
+import pytest
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@pytest.fixture
+def bot_token() -> str:
+    return os.getenv("tg_token")
+
+
+def test_bot_connection(bot_token) -> None:
+    assert bot_token is not None
--- bot/tg_bot.py
+++ bot/tg_bot.py
@@ -1,6 +1,31 @@
-def main():
-    pass
+import asyncio
+import logging
+
+from bot.config import Config
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command
+
+CONFIG = Config()
+DISPATCHER = Dispatcher()
+BOT = Bot(token=CONFIG.tg_token)
+logging.basicConfig(level=logging.INFO)
+
+
+@DISPATCHER.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(
+        text="Привет! Я - бот, который в любой момент может подсказать содержание одной из книг, о которых я знаю"
+    )
+
+
+@DISPATCHER.message(F.text)
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+
+
+async def main():
+    await DISPATCHER.start_polling(BOT)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
